fbbeb803b39632d0866daff7758b84b6dca38939
Felix <felix.hahnlein@ensimag.grenoble-inp.fr>
2016-02-08 13:58:06 +0100

    ok

--- TP2_hahnleif_reverdea/src/test_operator_self_subtraction.cpp
+++ TP2_hahnleif_reverdea/src/test_operator_self_subtraction.cpp
@@ -16,5 +16,20 @@ int main()
     assert(x.pCor[0] == 0.5);
     assert(x.pCor[1] == 0.5);
     assert(x.pCor[2] == 0.5);
+    x -= x;
+    assert(x.pCor[0] == 0);
+    assert(x.pCor[1] == 0);
+    assert(x.pCor[2] == 0);
+    //ATTENTION MEME ERREUR QUE DANS ADDITION
+    //ON S'ATTEND A 6 ET ON OBTIENT 0
+    y -= -y;
+    std::cout<<"y.pCor[0] : "<<y.pCor[0]<<std::endl;
+    std::cout<<"y.pCor[1] : "<<y.pCor[1]<<std::endl;
+    std::cout<<"y.pCor[2] : "<<y.pCor[2]<<std::endl;
+    /*
+    assert(y.pCor[0] == 6);
+    assert(y.pCor[1] == 6);
+    assert(y.pCor[2] == 6);
+    */
     std::cout<<"OK"<<std::endl;
 }
